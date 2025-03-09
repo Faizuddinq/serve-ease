448cdc202c81c1cc3792327776ab8ec170cfd4b3
Faizuddinq <faizqureshi6126@gmail.com>
2025-03-09 15:50:46 +0530

    setting up the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+  return (
+    <>
+    <div className=" bg-red-100 h-screen w-screen flex items-center justify-center">
+     <p>HIII SERVE EASE</p>
+
+    </div>
+    </>
+  )
+}
+
+export default App
