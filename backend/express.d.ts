349ee0be313ef981ab20637abbe853d252ff7ead
Faizuddinq <faizqureshi6126@gmail.com>
2025-03-18 00:45:07 +0530

    completing code

--- backend/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-import { IUser } from "../models/userModel"; // Import User Type
-
-// Extend Express Request Type
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: IUser; // Attach user object to Request
-  }
-}
--- frontend/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Home } from './Home.tsx'
-export { default as Orders } from './Orders.tsx'
-export { default as Auth } from './Auth.tsx'
-export { default as Tables } from './Tables.tsx'
-export { default as Menu } from './Menu.tsx'
-export { default as Dashboard } from "./Dashboard.tsx"
